76f5096733df049e79c15579ff96721b6553bf81
adrianstephens <adrianstephens@msn.com>
2026-02-28 13:39:40 -0800

    as16, etc

--- src/utils.ts
+++ src/utils.ts
@@ -75,23 +75,112 @@ export function splitBinary(n : number | bigint, splits : number[]) {
 //-----------------------------------------------------------------------------
 //	buffers
 //-----------------------------------------------------------------------------
-
 export const isLittleEndian = (new Uint8Array(new Uint16Array([0x1234]).buffer))[0] === 0x34;
 
-interface arrayBuffer {
+export interface arrayBuffer<T = any> {
 	length:			number;
 	buffer:			ArrayBufferLike;
 	byteLength:		number;
 	byteOffset:		number;
-	slice(begin:	number, end?: number): arrayBuffer;
-    [n: number]:	number;
+	slice(begin:	number, end?: number): arrayBuffer<T>;
+	subarray(begin: number, end?: number): arrayBuffer<T>;
+    [n: number]:	T;
+	[Symbol.iterator](): IterableIterator<T>;
 }
 
-function dupBuffer8(arg: arrayBuffer) {
-	const b = new Uint8Array(arg.byteLength);
-	b.set(new Uint8Array(arg.buffer, arg.byteOffset, arg.byteLength));
-	return b;
+export type bufferView<T extends arrayBuffer<R>, R> = (new(a: ArrayBufferLike, offset?: number, length?: number)=>T) & {BYTES_PER_ELEMENT?: number};
+
+type DataViewType = 'Uint16' | 'Uint32' | 'BigUint64' | 'Int16' | 'Int32' | 'BigInt64' | 'Float32' | 'Float64';
+type DataViewTypeGet<T extends DataViewType> = `get${T}`;
+type DataViewTypeSet<T extends DataViewType> = `set${T}`;
+type DataViewReturnType<T extends DataViewType> = T extends 'BigUint64' ? bigint : T extends 'BigInt64' ? bigint : number;
+
+const bufferViews: Record<DataViewType, [bufferView<arrayBuffer, any>, number]> = {
+	Uint16: 	[Uint16Array,		2],
+	Int16: 		[Int16Array,		2],
+	Uint32: 	[Uint32Array,		4],
+	Int32: 		[Int32Array,		4],
+	BigUint64: 	[BigUint64Array,	8],
+	BigInt64: 	[BigInt64Array,		8],
+	Float32: 	[Float32Array,		4],
+	Float64: 	[Float64Array,		8],
+} as const;
+
+function as<T extends DataViewType>(arg: arrayBuffer, type: T, be?: boolean) {
+	if (be !== isLittleEndian)
+		return new (bufferViews[type][0])(arg.buffer, arg.byteOffset, arg.byteLength);
+
+	const get		= `get${type}` as DataViewTypeGet<T>;
+	const set		= `set${type}` as DataViewTypeSet<T>;
+	const dv		= new DataView(arg.buffer, arg.byteOffset, arg.byteLength);
+	const stride	= bufferViews[type][1];
+	const littleEndian = be === undefined ? undefined : !be;
+
+	return new Proxy({
+		length:    	arg.length,
+		buffer:    	arg.buffer,
+		byteLength:	arg.byteLength,
+		byteOffset:	arg.byteOffset,
+		slice:     	(begin: number, end?: number) => arg.slice(begin, end),
+		subarray:  	(begin: number, end?: number) => arg.subarray(begin, end),
+	}, {
+		get(target, prop) {
+			if (prop in target)
+				return target[prop as keyof typeof target];
+
+			const index = Number(prop);
+			if (!isNaN(index) && index >= 0 && index < target.length)
+				return dv[get](index * stride, littleEndian);
+			return undefined;
+		},
+		set(target, prop, value: DataViewReturnType<T>) {
+			const index = Number(prop);
+			if (!isNaN(index) && index >= 0 && index < target.length) {
+				(dv[set] as any)(index * stride, value, littleEndian);
+				return true;
+			}
+			return false;
+		}
+	}) as arrayBuffer<DataViewReturnType<T>>;
 }
+
+// never copy, just reinterpret
+
+export function as8(arg: arrayBuffer) : Uint8Array;
+export function as8(arg?: arrayBuffer) : Uint8Array | undefined;
+export function as8(arg?: arrayBuffer) { return arg && new Uint8Array(arg.buffer, arg.byteOffset, arg.byteLength); }
+
+export function as16(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function as16(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function as16(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'Uint16', be); }
+
+export function as16s(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function as16s(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function as16s(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'Int16', be); }
+
+export function as32(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function as32(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function as32(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'Uint32', be); }
+
+export function as32s(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function as32s(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function as32s(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'Int32', be); }
+
+export function as64(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function as64(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function as64(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'BigUint64', be); }
+
+export function as64s(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function as64s(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function as64s(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'BigInt64', be); }
+
+export function asF32(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function asF32(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function asF32(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'Float32', be); }
+
+export function asF64(arg: arrayBuffer, be?: boolean) : arrayBuffer;
+export function asF64(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined;
+export function asF64(arg?: arrayBuffer, be?: boolean) : arrayBuffer | undefined { return arg && as(arg, 'Float64', be); }
 
 function dupBuffer(arg: arrayBuffer) {
 	const buffer = new ArrayBuffer(arg.byteLength);
@@ -127,9 +216,12 @@ function fixEndian64(a: ArrayBuffer, be?: boolean) {
 	return a;
 }
 
-export function to8(arg: arrayBuffer) : Uint8Array;
+// always copy to ensure we have a separate buffer
+
+export function to8(arg: arrayBuffer) : Uint8Array<ArrayBuffer>;
 export function to8(arg?: arrayBuffer) : Uint8Array | undefined;
-export function to8(arg?: arrayBuffer) { return arg && new Uint8Array(arg.buffer, arg.byteOffset, arg.byteLength); }
+//export function to8(arg?: arrayBuffer) { return arg && new Uint8Array(arg.buffer, arg.byteOffset, arg.byteLength); }
+export function to8(arg?: arrayBuffer) { return arg && new Uint8Array(dupBuffer(arg)); }
 
 export function to16(arg: arrayBuffer, be?: boolean) : Uint16Array;
 export function to16(arg?: arrayBuffer, be?: boolean) : Uint16Array | undefined;
@@ -166,7 +258,7 @@ export function findValue(buf: Uint8Array | undefined, value = 0, size = 1/*, be
 //	numbers
 //-----------------------------------------------------------------------------
 
-// get/put 1-7 byte integers from/to DavaView (7 bytes will lose precision)
+// get/put 1-7 byte integers from/to DataView (7 bytes will lose precision)
 
 export function getUint(dv: DataView, len: number, littleEndian?: boolean) {
 	let result = 0;
@@ -292,6 +384,8 @@ export function putBigUint(dv: DataView, v: bigint, len: number, littleEndian?: 
 //	text
 //-----------------------------------------------------------------------------
 
+export type TextEncoding = 'utf8' | 'utf16le' | 'utf16be';
+
 export function stringCode(s: string) {
 	let r = 0;
 	for (let i = 0; i < s.length; i++)
@@ -305,18 +399,24 @@ export function stringCodeBig(s: string) {
 	return r;
 }
 
-export type TextEncoding = 'utf8' | 'utf16le' | 'utf16be';
-
 export function encodeText(str: string, encoding: TextEncoding = 'utf8', bom = false): Uint8Array {
 	if (bom)
 		str = String.fromCharCode(0xfeff) + str;
 
-	if (encoding === 'utf16be') {
-		const b = Buffer.from(str, 'utf16le');
-		pairSwap(b);
-		return b;
-	}
-	return Buffer.from(str, encoding);
+	if (encoding === 'utf8')
+		return new TextEncoder().encode(str);
+	
+	// utf16le or utf16be
+	const len = str.length;
+	const buf = new Uint8Array(len * 2);
+	const view = new Uint16Array(buf.buffer);
+	for (let i = 0; i < len; i++)
+		view[i] = str.charCodeAt(i);
+	
+	if (encoding === 'utf16be')
+		pairSwap(buf);
+	
+	return buf;
 }
 
 export function encodeTextInto(str: string, into: Uint8Array, encoding: TextEncoding, bom = false) {
@@ -324,16 +424,19 @@ export function encodeTextInto(str: string, into: Uint8Array, encoding: TextEnco
 }
 
 export function decodeText(buf: Uint8Array, encoding: TextEncoding = 'utf8'): string {
-	if (encoding === 'utf16be') {
-		buf = dupBuffer8(buf);
-		pairSwap(buf);
-		encoding = 'utf16le';
-	}
-	return Buffer.from(buf).toString(encoding);
+	if (encoding === 'utf8')
+		return new TextDecoder('utf-8').decode(buf);
+	
+	// utf16le (or swapped utf16be)
+	const view = as16(buf, encoding === 'utf16be');	
+	let result = '';
+	for (let i = 0; i < view.length; i += 8192)
+		result += String.fromCharCode(...view.subarray(i, i + 8192));
+	return result;
 }
 
 export function decodeTextTo0(buf: Uint8Array | undefined, encoding: TextEncoding = 'utf8'): string {
-	return buf ? decodeText(buf.subarray(0, encoding === 'utf8' ? buf.indexOf(0) : (new Uint16Array(buf)).indexOf(0) * 2), encoding) : '';
+	return buf ? decodeText(buf.subarray(0, encoding === 'utf8' ? buf.indexOf(0) : (new Uint16Array(buf.buffer, buf.byteOffset, buf.byteLength / 2)).indexOf(0) * 2), encoding) : '';
 }
 
 export function getTextEncoding(bytes: Uint8Array): TextEncoding {
